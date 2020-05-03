20139c8889bde9ad26bc0fff91ed6522a8e31396
Vikash Singh <vikash.singh@techverito.com>
2020-05-03 23:43:59 +0530

    Updates

---
+++ precache-manifest.5bf8f4b23556d9baa788087351c3b039.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bdd2c1ad201bdc0ae5df10502bed9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a3c9b825a5380c9ba3c",
+    "url": "/static/css/2.d409d1d9.chunk.css"
+  },
+  {
+    "revision": "6b297d1afb001a075154",
+    "url": "/static/css/main.c5b5f07c.chunk.css"
+  },
+  {
+    "revision": "6a3c9b825a5380c9ba3c",
+    "url": "/static/js/2.eeb1b8dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.eeb1b8dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b297d1afb001a075154",
+    "url": "/static/js/main.c472b706.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
